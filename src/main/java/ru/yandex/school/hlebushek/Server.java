2ba84bd3850b71444ca4991c2326fcd47c9d7b08
Borovkov <aleksmail85@mail.ru>
2014-11-19 16:29:17 +0300

    create draft api resource

--- src/main/java/ru/yandex/school/hlebushek/Server.java
+++ src/main/java/ru/yandex/school/hlebushek/Server.java
@@ -9,14 +9,12 @@ import javax.ws.rs.core.FeatureContext;
 public class Server extends ResourceConfig {
 
     public Server() {
-
         register(new DynamicFeature() {
             @Override
             public void configure(ResourceInfo resourceInfo, FeatureContext context) {
                 //context.register(DatabaseProvider.class); todo: add db provider class
             }
         });
-
         packages(Server.class.getPackage().getName());
     }
 }
