9a6b02926fc29c4c7ec3583636a23151c4e5fb86
Borovkov <aleksmail85@mail.ru>
2014-11-20 11:19:33 +0300

    use jackson for json media

--- src/main/java/ru/yandex/school/hlebushek/Server.java
+++ src/main/java/ru/yandex/school/hlebushek/Server.java
@@ -1,17 +1,19 @@
 package ru.yandex.school.hlebushek;
 
-import org.glassfish.jersey.moxy.json.MoxyJsonFeature;
+import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 
 import javax.ws.rs.container.DynamicFeature;
 import javax.ws.rs.container.ResourceInfo;
 import javax.ws.rs.core.FeatureContext;
+
 import ru.yandex.school.hlebushek.service.DatabaseProvider;
 
 public class Server extends ResourceConfig {
 
     public Server() {
-        register(MoxyJsonFeature.class);
+
+        register(JacksonFeature.class);
         register(new DynamicFeature() {
             @Override
             public void configure(ResourceInfo resourceInfo, FeatureContext context) {
