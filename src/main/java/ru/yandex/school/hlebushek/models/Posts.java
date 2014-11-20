f1a506761c859c73cbdfbf184b162289bd959507
Borovkov <aleksmail85@mail.ru>
2014-11-20 17:45:51 +0300

    merge branch models_Sergey

--- src/main/java/ru/yandex/school/hlebushek/models/Posts.java
+++ src/main/java/ru/yandex/school/hlebushek/models/Posts.java
@@ -2,6 +2,45 @@ package ru.yandex.school.hlebushek.models;
 
 import org.javalite.activejdbc.Model;
 
+import java.sql.Date;
+
 public class Posts extends Model {
 
+    public void setTitle(String title) {
+        setString("title", title);
+    }
+    public String getTitle() {
+        return getString("title");
+    }
+
+    public void setMessage(String message) {
+        setString("message", message);
+    }
+    public String getMessage() {
+        return getString("message");
+    }
+
+    public void setAuthorId(int id) {
+        setInteger("author_id", id);
+    }
+
+    public int getAuthorId() {
+        return getInteger("author_id");
+    }
+
+    public Date getCreateDate() {
+        return getDate("create_date");
+    }
+
+    public Date getLastUpdateDate() {
+        return getDate("modified_date");
+    }
+
+    public boolean getIsDeleted() {
+        return getBoolean("is_deleted");
+    }
+
+    public void setIsDeleted(boolean isDeleted) {
+        setBoolean("is_deleted",isDeleted);
+    }
 }
