99f92f331e19f3aa2d09e67f8247628e7ab0f7b5
ksevasilyeva <ksevasilyeva@gmai.com>
2014-11-21 21:40:24 +0300

    test_interface_added

---
+++ src/main/webapp/public/js/app.js
@@ -0,0 +1,24 @@
+
+var app = angular.module('blogWebApp', [
+    'ngRoute'
+]);
+
+
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+        .when("/", {templateUrl: "partials/home.html", controller: "TestController"})
+        .when("/about", {templateUrl: "partials/about.html", controller: "TestController"})
+        .when("/all_posts", {templateUrl: "partials/all_posts.html", controller: "PostListController"})
+        .when("/all_posts/user:userId", {templateUrl: "partials/all_posts.html", controller: "PostListController"})
+        .when("/add_post", {templateUrl: "partials/add_post.html", controller: "TestController"})
+
+        .otherwise("/404", {templateUrl: "partials/404.html", controller: "TestController"});
+}]);
+
+
+
+app.controller('TestController', function () {
+    console.log("Just a test controller");
+
+});
+
