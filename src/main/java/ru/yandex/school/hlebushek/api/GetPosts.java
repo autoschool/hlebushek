e9807857522cc915a89c3a9d04d5a2f2cdd27173
aleks <aleksmail85@mail.ru>
2014-11-22 12:40:01 +0300

    update api & posts model

--- src/main/java/ru/yandex/school/hlebushek/api/GetPosts.java
+++ src/main/java/ru/yandex/school/hlebushek/api/GetPosts.java
@@ -1,5 +1,7 @@
 package ru.yandex.school.hlebushek.api;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import org.javalite.activejdbc.LazyList;
 import ru.yandex.school.hlebushek.models.Posts;
 
@@ -12,16 +14,55 @@ import javax.ws.rs.core.MediaType;
 @Path("GetPosts")
 public class GetPosts {
 
+    private JsonArray array = new JsonArray();
+
+    /**
+     * Method return json response posts model
+     * @param postId int { /GetPosts?post-id=num }
+     * @param authorId int { /GetPosts?author-id=num }
+     * @return JsonArray by String
+     */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public String getPosts(@QueryParam("author-id") int userId, @QueryParam("post-id") int postId) {
-        if (userId != 0) {
-            LazyList<Posts> list = Posts.where(String.format("author_id = '%s'", userId));
-            return list.toJson(true);
-        } else if (postId != 0) {
-            LazyList<Posts> list = Posts.where(String.format("post_id = '%s'", postId));
-            return list.toJson(true);
+    public String getPosts(
+            @QueryParam("post-id") int postId,
+            @QueryParam("author-id") int authorId) {
+        if (postId != 0 && authorId == 0) {
+            try {
+                Posts post = Posts.findById(postId);
+                array = setJsonArrayPosts(post);
+            } catch (NullPointerException e) {
+                System.out.println(String.format("Post_id = '%s' not found", postId));
+            }
         }
-        return Posts.findAll().toJson(true);
+        if (postId ==0 && authorId != 0) {
+            LazyList<Posts> posts = Posts.where(String.format("author_id = '%s'", authorId));
+            array = setJsonArrayPosts(posts);
+        }
+        return array.toString();
+    }
+
+    private JsonArray setJsonArrayPosts(LazyList<Posts> posts) {
+        for (Posts post : posts) {
+            array.add(setJsonObjectPost(post));
+        }
+        return array;
+    }
+
+    private JsonArray setJsonArrayPosts(Posts post) {
+        array.add(setJsonObjectPost(post));
+        return array;
+    }
+
+    private JsonObject setJsonObjectPost(Posts post) {
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("post_id", post.getPostId());
+        jsonObject.addProperty("title", post.getTitle());
+        jsonObject.addProperty("message", post.getMessage());
+        jsonObject.addProperty("author_id", post.getAuthorId());
+        jsonObject.addProperty("create_date", post.getCreateDate());
+        jsonObject.addProperty("modified_date", post.getModifiedDate());
+        jsonObject.addProperty("is_deleted", post.getIsDeleted());
+        return jsonObject;
     }
 }
--- src/main/java/ru/yandex/school/hlebushek/models/Posts.java
+++ src/main/java/ru/yandex/school/hlebushek/models/Posts.java
@@ -1,17 +1,26 @@
 package ru.yandex.school.hlebushek.models;
 
 import org.javalite.activejdbc.Model;
+import org.javalite.activejdbc.annotations.IdName;
+import org.javalite.activejdbc.annotations.Table;
 
 import java.sql.Date;
 
+@Table("Posts")
+@IdName("post_id")
 public class Posts extends Model {
 
+    final static private String POST_ID = "post_id";
     final static private String TITLE = "title";
     final static private String MESSAGE = "message";
     final static private String AUTHOR_ID = "author_id";
     final static private String CREATE_DATE = "create_date";
     final static private String MODIFIED_DATE = "modified_date";
     final static private String IS_DELETED = "is_deleted";
+
+    public int getPostId() {
+        return getInteger(POST_ID);
+    }
 
     public String getTitle() {
         return getString(TITLE);
@@ -33,12 +42,24 @@ public class Posts extends Model {
         return getInteger(AUTHOR_ID);
     }
 
-    public Date getCreateDate() {
-        return getDate(CREATE_DATE);
+    public void setAuthorId(int authorId) {
+        setInteger(AUTHOR_ID, authorId);
     }
 
-    public Date getLastUpdateDate() {
-        return getDate(MODIFIED_DATE);
+    public String getCreateDate() {
+        if (getDate(CREATE_DATE) != null) {
+            return getDate(CREATE_DATE).toString();
+        } else {
+            return null;
+        }
+    }
+
+    public String getModifiedDate() {
+        if (getDate(MODIFIED_DATE) != null) {
+            return getDate(MODIFIED_DATE).toString();
+        } else {
+            return null;
+        }
     }
 
     public boolean getIsDeleted() {
@@ -46,6 +67,6 @@ public class Posts extends Model {
     }
 
     public void setIsDeleted(boolean isDeleted) {
-        setBoolean(IS_DELETED,isDeleted);
+        setBoolean(IS_DELETED, isDeleted);
     }
 }
