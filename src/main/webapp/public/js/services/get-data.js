39f53721d97e7805af33b79e16f466be443839c7
drak_kin <kobyshev.sergey@gmail.com>
2014-11-28 01:12:48 +0400

    delete api folder, delete scripts before pull request

--- src/main/webapp/public/js/services/get-data.js
+++
@@ -1,8 +0,0 @@
-app.service('GetPosts', ['$http', function ($http) {
-    var url = 'service/GetPosts';
-    this.call = function (params) {
-        return $http.get(url, {
-            params: params
-        });
-    };
-}]);
